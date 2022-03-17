ebed22e3e0ccf99dfd83de28690f4da18755d58d
Mick Vleeshouwer <mick@imick.nl>
2022-03-17 16:39:01 +0000

    Bugfixes

--- custom_components/sagemcom_fast/device_tracker.py
+++ custom_components/sagemcom_fast/device_tracker.py
@@ -19,8 +19,6 @@ from sagemcom_api.models import Device
 
 from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up from config entry."""
@@ -28,7 +26,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
 
     async_add_entities(
-        SagemcomScannerEntity(coordinator, idx, config_entry.entry_id)
+        SagemcomScannerEntity(coordinator, idx)
         for idx, device in coordinator.data.items()
     )
 
